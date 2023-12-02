abda446c654c7c1915c757e8f10e51e7e767281a
codetrsh <134286416+codetrsh@users.noreply.github.com>
2023-12-02 15:09:34 -0300

    13

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-// const PORT = 3000 || process.env.PORT;
+const PORT = 3000 || process.env.PORT;
 
 const currentDate = new Date();
 const options = { 
@@ -56,6 +56,6 @@ app.get('/api/v1/:id', (req, res) => {
     res.status(200).json({param: req.params.id})
 })
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running at PORT: ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT: ${PORT}`);
+});
